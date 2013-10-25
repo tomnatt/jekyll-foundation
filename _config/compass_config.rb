1dadaf05d0597220f87fd39f6f251c11ba3af363
tomnatt <github@tomnatt.com>
2013-10-25 19:10:34 +0100

    shift to guard and moved foundation part 2

---
+++ _config/compass_config.rb
@@ -0,0 +1,8 @@
+css_dir = 'css'
+sass_dir = '_assets'
+additional_import_path = 'foundation'
+images_dir = 'images'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
